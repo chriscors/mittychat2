0607d06f04dfb384fc108f67c62aefe81838e9cf
Chris Corsi <chris.corsi@proofgeist.com>
2024-12-07 13:46:54 -0500

    add dapi and schema

---
+++ config/schema/client/index.ts
@@ -0,0 +1,1 @@
+  export { client as studentLayout } from "./student";
---
+++ fmschema.config.mjs
@@ -0,0 +1,17 @@
+/**
+ * @type {import("@proofgeist/fmdapi/dist/utils/typegen/types.d.ts").GenerateSchemaOptions}
+ */
+export const config = {
+  clientSuffix: "Layout",
+  schemas: [
+    // add your layouts and name schemas here
+    { layout: "student", schemaName: "student" },
+
+    // repeat as needed for each schema...
+    // { layout: "my_other_layout", schemaName: "MyOtherSchema" },
+  ],
+
+  // change this value to generate the files in a different directory
+  path: "config/schema",
+  clearOldFiles: true,
+};
