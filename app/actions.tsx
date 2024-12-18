6a668c17c3c4223d4b0e13fa6dd4b69ed36d3b41
Chris Corsi <chris.corsi@proofgeist.com>
2024-12-17 22:03:58 -0500

    Set up local ollama to work

--- app/actions.tsx
+++ app/actions.tsx
@@ -1,12 +1,19 @@
 "use server";
 
-import { createAI, getMutableAIState, streamUI } from "ai/rsc";
-import { createOllama } from "ollama-ai-provider"
+import {
+  createAI,
+  createStreamableUI,
+  createStreamableValue,
+  getMutableAIState,
+  streamUI,
+} from "ai/rsc";
+import { createOllama } from "ollama-ai-provider";
 import { ReactNode } from "react";
 import { z } from "zod";
 import { nanoid } from "nanoid";
 import { groq } from "@ai-sdk/groq";
-import { studentLayout } from "@/config/schema/client/index";
+import { StudentLayout } from "@/config/schema/client/index";
+import { streamText, tool } from "ai";
 
 export interface ServerMessage {
   role: "user" | "assistant";
@@ -22,165 +29,66 @@ export interface ClientMessage {
 export async function continueConversation(
   input: string
 ): Promise<ClientMessage> {
-  "use server";
+  ("use server");
 
   const aiState = getMutableAIState();
 
+  aiState.update([
+    ...aiState.get(),
+    {
+      id: nanoid(),
+      role: "user",
+      content: input,
+    },
+  ]);
+
+  const messageStream = createStreamableUI(null);
+
   const ollama = createOllama({
-    baseURL: "https://ollama.e7f9.ottomatic.cloud/ollama/api/",
+    baseURL: "https://api.vultrinference.com/v1/",
     headers: {
       Authorization: `Bearer ${process.env.OLLAMA_API_KEY}`,
     },
   });
 
-  const result = await streamUI({
-    model: groq("llama3-8b-8192"), //ollama("codellama:13b"),
+  const result = await streamText({
+    model: ollama("smollm2:latest"), //groq("llama3-8b-8192"),
     system:
-      "You are an AI assistant that can create and retrieve students from a database and answer questions about them. You may use a tool, but you should not always use a tool. Confirm with the user before using a tool.",
+      "You are an AI assistant that can create and retrieve student information from a database and answer questions about them.",
     messages: [...aiState.get(), { role: "user", content: input }],
-    text: ({ content, done }) => {
-      if (done) {
-        aiState.done((messages: ServerMessage[]) => [
-          ...messages,
-          { role: "assistant", content },
-        ]);
-      }
-
-      return <div>{content}</div>;
-    },
+    temperature: 0,
     tools: {
-      createStudent: {
-        description: "Create a new student",
-        parameters: z.object({
-          nameFirst: z.string().describe("The first name of the student"),
-          nameLast: z.string().describe("The last name of the student"),
-          email: z.string().optional().describe("The email of the student"),
-          phoneNumber: z
-            .string()
-            .optional()
-            .describe(
-              "The phone number of the student in (123) 456-7890 format"
-            ),
-          dob: z
-            .string()
-            .optional()
-            .describe("The date of birth of the student in MM/DD/YYYY format"),
-          address: z.string().optional().describe("The address of the student"),
-          graduationYear: z
-            .optional(z.string())
-            .describe("The graduation year of the student"),
-        }),
-        generate: async function* ({
-          nameFirst,
-          nameLast,
-          email,
-          phoneNumber,
-          dob,
-          address,
-          graduationYear,
-        }) {
-          console.log("Creating student", nameFirst, nameLast);
-          const student = await studentLayout.create({
-            fieldData: {
-              nameFirst,
-              nameLast,
-              email,
-              phoneNumber,
-              dob,
-              address,
-              graduationYear,
-            },
-          });
-          if (student) {
-            return (
-              <div>
-                {nameFirst} {nameLast} created!
-              </div>
-            );
-          }
-          return <div>Error creating student</div>;
-        },
-      },
-      getStudent: {
-        description: "Get data about a student based on their information",
-        parameters: z.object({
-          nameFirst: z
-            .string()
-            .optional()
-            .describe("The first name of the student"),
-          nameLast: z
-            .string()
-            .optional()
-            .describe("The last name of the student"),
-          email: z.string().optional().describe("The email of the student"),
-          phoneNumber: z
-            .string()
-            .optional()
-            .describe(
-              "The phone number of the student in (123) 456-7890 format"
-            ),
-          dob: z
-            .string()
-            .optional()
-            .describe("The date of birth of the student in MM/DD/YYYY format"),
-          address: z.string().optional().describe("The address of the student"),
-          graduationYear: z
-            .string()
-            .optional()
-            .describe("The graduation year of the student"),
-        }),
-        generate: async function* ({
-          nameFirst,
-          nameLast,
-          email,
-          phoneNumber,
-          dob,
-          address,
-          graduationYear,
-        }) {
-          const student = await studentLayout.find({
-            query: {
-              nameFirst,
-              nameLast,
-              email,
-              phoneNumber,
-              dob,
-              address,
-              graduationYear,
-            },
-          });
-          if (student) {
-            return (
-              <div>
-                {student.data.map((student) => (
-                  <div>
-                    {student.fieldData.nameFirst} {student.fieldData.nameLast}
-                    <br />
-                    {student.fieldData.email}
-                    <br />
-                    {student.fieldData.phoneNumber}
-                    <br />
-                    {student.fieldData.dob}
-                    <br />
-                    {student.fieldData.address}
-                    <br />
-                    {student.fieldData.graduationYear}
-                  </div>
-                ))}
-              </div>
-            );
-          }
-          return <div>No student found</div>;
-        },
-      },
+      getStudentTool,
     },
     toolChoice: "auto",
+    maxSteps: 10,
   });
+  let textContent = "";
+  for await (const delta of result.fullStream) {
+    const { type } = delta;
+    if (type === "text-delta") {
+      const { textDelta } = delta;
+      textContent += textDelta;
+      messageStream.update(<div>{textContent}</div>);
 
+      aiState.update([
+        ...aiState.get(),
+        {
+          id: nanoid(),
+          role: "assistant",
+          content: textContent,
+        },
+      ]);
+    } else if (type === "tool-call") {
+      const { toolName, args } = delta;
+      console.log("Tool call", toolName, args);
+    }
+  }
+  messageStream.done();
   return {
     id: nanoid(),
     role: "assistant",
-    display: result.value,
+    display: messageStream.value,
   };
 }
 
@@ -209,11 +117,30 @@ export type UIState = {
   attachments?: React.ReactNode;
 }[];
 
-
 export const AI = createAI<ServerMessage[], ClientMessage[]>({
   actions: {
     continueConversation,
   },
   initialAIState: [],
   initialUIState: [],
+});
+
+const getStudentTool = tool({
+  description:
+    "Get data about students based on their information. Can also return the count of students that match the query.",
+  parameters: z.object({
+    nameFirst: z.string().optional().describe("The first name of the student"),
+    nameLast: z.string().optional().describe("The last name of the student"),
+  }),
+  execute: async ({ nameFirst, nameLast }) => {
+    return {
+      firstName: "john",
+      lastName: "doe",
+      email: "john.doe@example.com",
+      phoneNumber: "1234567890",
+      dob: "01/01/2000",
+      address: "1234567890",
+      graduationYear: "2024",
+    };
+  },
 });
